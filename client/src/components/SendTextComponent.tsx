0404db38f5cbd0241fd8ce4da4b0c74c716c8c66
hungryato <hungryato17@icloud.com>
2025-02-12 14:51:49 +0900

    feat: Update SendTextComponent to use axios then for server communication
    - Use axios then to send POST request
    - Handle server response and display it on the screen

--- client/src/components/SendTextComponent.tsx
+++ client/src/components/SendTextComponent.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const SendTextComponent: React.FC = () => {
     const [text, setText] = useState('');
@@ -7,15 +8,14 @@ const SendTextComponent: React.FC = () => {
     const handleSubmit = async (event: React.FormEvent) => {
         console.log('Text submitted:', text);
         event.preventDefault();
-        const response = await fetch('http://localhost:3001/send-text', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ text })
-        });
-        const data = await response.text();
-        setResponse(data);
+        axios.post('http://localhost:3001/send-text', { text })
+            .then((res) => {
+                setResponse(res.data);
+            })
+            .catch((error) => {
+                console.error('Error sending text:', error);
+                setResponse('Error sending text');
+            });
     };
 
     return (
